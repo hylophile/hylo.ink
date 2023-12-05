f2b8a181fdf45092610f7a9b8c1f53ddf247580d
hylo <hylo@posteo.de>
2023-12-05 16:00:51 +0100

    feat: initial commit

---
+++ hyloink.clj
@@ -0,0 +1,26 @@
+;; mypage.bb.clj
+;; <img src="http://localhost:3001/display?w=1000&path=2023-12-03.kra.png&op=resize" />
+(ns hyloink
+  (:require [babashka.fs :as fs])
+  (:require [babashka.process :as p :refer [process]]
+            [hiccup2.core :as h]))
+
+(def imgs (into [] (map str (fs/list-dir "./export"))))
+;; (def imgs (into [] (map str (fs/glob "/home/n/art/export" "*.png"))))
+(defn img [s]
+  [:a {:href s}
+   [:img {:src s :width "250px"}]])
+
+(def out (str (h/html
+               [:head
+                [:meta {:charset "utf-8"}]
+                [:script {:src "https://cdn.tailwindcss.com"}]]
+               [:div.grid.grid-cols-2.gap-2
+                (map img imgs)]
+
+               [:ul
+                      (for [x (range 1 9)]
+                        [:li x])])))
+
+
+(spit "./index.html" out)
