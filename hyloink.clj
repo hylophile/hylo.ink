10a51454ceebe7e5c8c7e93fe068a2e66c8bfbc4
hylo <hylo@posteo.de>
2023-12-06 20:14:20 +0100

    refactor: move back to .png

--- hyloink.clj
+++ hyloink.clj
@@ -1,11 +1,10 @@
 ;; mypage.bb.clj
 ;; <img src="http://localhost:3001/display?w=1000&path=2023-12-03.kra.png&op=resize" />
 (ns hyloink
-  (:require [babashka.fs :as fs])
-  (:require [babashka.process :as p :refer [process]]
+  (:require [babashka.fs :as fs]
             [hiccup2.core :as h]))
 
-(def imgs (->> (fs/glob "./export" "*.jpg" {:follow-links true})
+(def imgs (->> (fs/glob "./export" "*.png" {:follow-links true})
                (map fs/file-name)
                sort
                reverse))
@@ -15,7 +14,7 @@
    [:a {:href (format "/art/api/fullhd/%s" s)}
     [:img.mx-auto.transition.ease-in-out {:class "hover:scale-105" :src (format "/art/api/thumbnail/%s" s)}]]
    [:figcaption.text-center.pt-2
-    (fs/strip-ext s {:ext "kra.jpg"})]])
+    (fs/strip-ext s {:ext "kra.png"})]])
 
 
 (defn menu-item [{:keys [active? name link]}]
